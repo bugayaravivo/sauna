d1e3f6b65409a1c3a77af6c009d209d128dd45b9
EC2 Default User <ec2-user@ip-172-31-14-221.ap-northeast-1.compute.internal>
2024-08-08 08:18:07 +0000

    buildingあらかた終了

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -8,17 +8,32 @@ class BuildingsController < ApplicationController
     @building.user_id = current_user.id
     @building.save
     redirect_to buildings_path
-  end 
+  end
 
   def index
+    @building = Building.new
+    @buildings = Building.all
   end
 
   def show
+    @building = Building.find(params[:id])
   end
   
-  private
+  def destroy
+    @building = Building.find(params[:id])
+    @building.destroy
+    redirect_to buildings_path
+  end 
   
+  def edit
+  end 
+  
+  def update
+  end 
+
+  private
+
   def building_params
-    params.require(:building).permit(:shop_name, :place, :body)
+    params.require(:building).permit(:shop_name, :image, :place, :body)
   end
 end
